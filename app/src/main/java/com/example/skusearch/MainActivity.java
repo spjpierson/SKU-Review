4c7e3ee3b4022a8f8be8a5270b9d2565ad4b80a0
spjpi <spjpierson@gmail.com>
2024-01-27 09:23:29 -0600

    Created Click Listener

--- app/src/main/java/com/example/skusearch/MainActivity.java
+++ app/src/main/java/com/example/skusearch/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.skusearch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,5 +37,27 @@ public class MainActivity extends AppCompatActivity {
         search_database_button = findViewById(R.id.database_search_button);
 
         history_container = findViewById(R.id.history_container);
+
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Toast.makeText(getApplicationContext(), "Camera button was press", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        search_online_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Search Online Button was press", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        search_database_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Search Database was press", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
